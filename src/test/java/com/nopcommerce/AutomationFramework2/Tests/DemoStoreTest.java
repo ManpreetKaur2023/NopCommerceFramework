036b4366f495d3e6cb326241d1cba8b635dd15b9
ManpreetKaur2023 <129234886+ManpreetKaur2023@users.noreply.github.com>
2023-07-09 14:04:13 -0400

    Add files via upload

--- src/test/java/com/nopcommerce/AutomationFramework2/Tests/DemoStoreTest.java
+++ src/test/java/com/nopcommerce/AutomationFramework2/Tests/DemoStoreTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import com.nopcommerce.AutomationFramework2.Base.TestBase;
@@ -26,6 +27,7 @@ public class DemoStoreTest extends TestBase {
 	}
 
 	@Test(priority = 1)
+	@Ignore
 	public void validateUserRegister() {
 
 		register = demoStore.clickRegisterBtn();
